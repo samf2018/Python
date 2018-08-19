a1893246bb034a67cdd0ae48c4be7220b3367b79
samf2018 <34931360+samf2018@users.noreply.github.com>
2018-08-19 16:56:24 -0400

    Add files via upload

---
+++ MadLibs Game Python.py
@@ -0,0 +1,12 @@
+
+# print("Lets Play The Mad Libs \n Game!")
+
+Favorite_Color= input("Enter A Color:")
+Name= input("Enter A Name:")
+Name_2= input("Enter Another Name:")
+Country= input("Enter Your Country:")
+
+print("When I think of a beautiful place, I think of "  + Country + ".")
+print("My friend "   + Name +   " is a clue that I was born in "  + Country +  ".")
+print("When I think of a beautiful color "  + Favorite_Color +  " comes to mind "+ ".")
+print("Fun fact: I love " + Name_2 + " so much " + "." )
